e066822358a2c75d5116701a638d17785e66d2be
unknown <156849177+angelgarciasolorzano@users.noreply.github.com>
2025-01-06 12:24:39 -0600

    remove animaciones, types de los dropdown

--- client/src/components/dropdown/dropdownType.ts
+++ client/src/components/dropdown/dropdownType.ts
@@ -1,10 +1,5 @@
 import { IconType } from "react-icons";
 
-export enum DropdownType {
-  Profile = "Profile",
-  Notificacion = "Notificacion",
-};
-
 export interface DropdownItems {
   text: string;
   icon?: IconType;
