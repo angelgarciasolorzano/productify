03f85d2fe4be62ce9ddad1758f4e7e190ccfffbb
unknown <156849177+angelgarciasolorzano@users.noreply.github.com>
2025-01-11 16:53:17 -0600

    change clases del modo oscuro y autocompletado

--- client/src/components/form/Input.tsx
+++ client/src/components/form/Input.tsx
@@ -22,23 +22,25 @@ function Input<T extends FieldValues>(props: Props<T>) {
         {labelName}
       </label>
 
-      <div className={`flex items-center w-full text-gray-700 bg-white border border-gray-400 
-        rounded-md shadow-sm overflow-hidden dark:text-gray-200 dark:bg-gray-700
+      <div className={`flex items-center w-full text-gray-700 border border-gray-400 
+        rounded-md shadow-sm overflow-hidden dark:text-gray-200
         ${errors
           ? `border-red-500 focus-within:ring-1 focus-within:ring-red-500`
           : `focus-within:border-blue-400 focus-within:ring-1 focus-within:ring-blue-400
             dark:focus-within:border-gray-200 dark:focus-within:ring-gray-200`
         }`}
       >
-        <div className="flex items-center justify-center px-2 text-gray-800 dark:text-gray-200">
+        <div className="flex items-center justify-center px-2 text-gray-800 
+          dark:text-gray-200"
+        >
           {Icon && <Icon className="w-5 h-5" />}
         </div>
 
         <input
           {...register(inputName)}
           {...inputProps}
-          className="w-full py-2 pr-2 border-0 focus:outline-none text-gray-800 dark:bg-gray-700 
-          dark:text-gray-200"
+          className="w-full py-2 pr-2 border-0 focus:outline-none text-gray-800 
+          bg-transparent dark:text-gray-200"
         />
       </div>
 
