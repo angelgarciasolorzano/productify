97f075932efbacfce55255fffdd1aefa9f3d6fc7
unknown <156849177+angelgarciasolorzano@users.noreply.github.com>
2025-03-02 19:43:29 -0600

    remove sincronizacion de modelos

--- server/src/validation/databaseValidation.ts
+++ server/src/validation/databaseValidation.ts
@@ -1,14 +1,9 @@
 import { sequelize } from "@/config";
-import "@/models/associations/productosAssociation";
-import "@/models/associations/usuariosAssociation";
 
 const conexionDatabase = async (): Promise<void> => {
   try{
     await sequelize.authenticate();
     console.log("Conexion a la base de datos establecida correctamente");
-
-    await sequelize.sync();
-    console.log("Modelos sincronizados");
   } catch (error) {
     console.error("Error en la conexion a la base de datos", error);
   }
