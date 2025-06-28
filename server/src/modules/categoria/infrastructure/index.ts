20824fc59ae84bdbd5d85119b2e265957da5f71e
unknown <156849177+angelgarciasolorzano@users.noreply.github.com>
2025-06-28 01:11:14 -0600

    add clase mapper en el modulo categoria
    - agregando index.ts en las carpetas de infraestructura y de dominio para las importaciones

---
+++ server/src/modules/categoria/infrastructure/index.ts
@@ -0,0 +1,3 @@
+export { default as CategoriaSequelize } from "./database/sequelize/categoria.model";
+
+export { default as CategoriaSequelizeMapper } from "./mappers/sequelize/categoria.mapper";
---
+++ server/src/modules/categoria/infrastructure/mappers/sequelize/categoria.mapper.ts
@@ -0,0 +1,62 @@
+import { Categoria } from "@/modules/categoria/domain";
+import { CategoriaSequelize } from "@/modules/categoria/infrastructure";
+
+/**
+ * @class CategoriaMapper
+ * @description Clase que mapea los objetos de dominio a los objetos de modelo Sequelize y viceversa
+*/
+class CategoriaMapper {
+  /**
+   * Metodo que mapea un objeto de modelo Sequelize a un objeto de dominio
+   * @param model Instancia del modelo Sequelize de Categoria
+   * @returns Objeto de la entidad de dominio Categoria
+   * @throws Error si no se puede mapear el modelo a un objeto de dominio
+  */
+  public static toDomain(model: CategoriaSequelize): Categoria {
+    if (!model) throw new Error("No se puede mapear un modelo Categoria nulo a un objeto de dominio");
+
+    return {
+      id: model.id_categoria,
+      nombre: model.nombre_categoria,
+      descripcion: model.descripcion_categoria,
+      estado: model.estado_categoria,
+      createdAt: model.createdAt,
+      updatedAt: model.updatedAt
+    };
+  };
+
+  /**
+   * Metodo que mapea un arreglo de objetos de modelo Sequelize a un arreglo de objetos de dominio
+   * @param categorias Arreglo de modelo Sequelize de Categoria
+   * @returns Arreglo de objetos de dominio Categoria
+   * @throws Error si no se puede mapear el modelo a un objeto de dominio
+  */
+  public static toDtoList(categorias: CategoriaSequelize[]): Categoria[] {
+    if (!categorias) throw new Error("No se puede mapear un array de modelos Categoria nulo a un array de dominio");
+
+    if (categorias.length === 0) return [];
+
+    return categorias.map(categoria => this.toDomain(categoria));
+  };
+
+  /**
+   * Metodo que mapea un objeto de dominio a un objeto de modelo Sequelize
+   * @param categoria Entidad de dominio Categoria
+   * @returns Objeto de modelo Sequelize de Categoria
+   * @throws Error si no se puede mapear el modelo a un objeto de dominio
+  */
+  public static toPersistence(categoria: Categoria): Partial<CategoriaSequelize> {
+    if (!categoria) throw new Error("No se puede mapear un objeto de dominio nulo a un modelo Categoria");
+
+    return {
+      id_categoria: categoria.id,
+      nombre_categoria: categoria.nombre,
+      descripcion_categoria: categoria.descripcion,
+      estado_categoria: categoria.estado,
+      createdAt: categoria.createdAt,
+      updatedAt: categoria.updatedAt
+    };
+  };
+};
+
+export default CategoriaMapper;
