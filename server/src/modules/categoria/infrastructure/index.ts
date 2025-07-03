8d1f8a7dab67497f62c40555a0d81dd02460ccb4
unknown <156849177+angelgarciasolorzano@users.noreply.github.com>
2025-07-03 10:23:30 -0600

    change el nombre del archivo mySql.model.ts a categoria.model.ts en la carpeta sequelize
    - actualizamos las importaciones y exportaciones de la carpeta infrastructure

--- server/src/modules/categoria/infrastructure/index.ts
+++ server/src/modules/categoria/infrastructure/index.ts
@@ -1,5 +1,5 @@
 export * from "./database/interface/categoria";
-export { default as CategoriaMySQlSequelize } from "./database/sequelize/mySql.model";
+export { default as CategoriaMySQlSequelize } from "./database/sequelize/categoria.model";
 
 export { default as CategoriaMapper } from "./mappers/categoria.mapper";
 
