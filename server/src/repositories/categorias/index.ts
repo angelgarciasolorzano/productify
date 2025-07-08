d7ca9f2dcce4056c6780a105e65da2e6e37cb34d
unknown <156849177+angelgarciasolorzano@users.noreply.github.com>
2025-07-08 09:44:03 -0600

    delete carpeta repositories
    - el repositories de categoria ya se encuenta implementado en el modulo de categoria
    - los demas repositories seran implementados en su respectivo modulo proximamente

--- server/src/repositories/categorias/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ICategoriaRepository } from "./iCategoriaRepository";
-export { default as CategoriaRepository } from "./categoriaRepository";
-export { default as CategoriaFinderRepository } from "./finder/categoriaFinder";
-export { default as CategoriaCRUDRepository } from "./crud/categoriaCRUD";
--- server/src/repositories/usuarios/mainRepository.ts
+++
@@ -1,9 +0,0 @@
-import authRepository from "./authRepository";
-import baseRepository from "./baseRepository";
-
-class MainRepository {
-  public base = baseRepository;
-  public auth = authRepository;
-};
-
-export default new MainRepository();
